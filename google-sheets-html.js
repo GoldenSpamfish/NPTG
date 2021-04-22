79744d93ae3ef55c8a1032b1dd47c7c463f85747
inthehouse333 <65719898+inthehouse333@users.noreply.github.com>
2021-04-22 09:31:18 -0400

    Create google-sheets-html.js
    The JavaScript required for the Table.html file to work.

---
+++ google-sheets-html.js
@@ -0,0 +1,22 @@
+google.charts.load('current', {packages: ['table']});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1v6i3MFlBfTom7p_WZHs-hK850D2pTg2kk4XJbrLlkd8/gviz/tq?output=html&usp=sharing');
+    query.setQuery('SELECT A, B, C, D label A "Scientific Name", B "Common Name", C "Link", D "Growth Form"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
