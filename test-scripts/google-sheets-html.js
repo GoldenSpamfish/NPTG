be707c5343a8740d5dbaef26eceea8b69e542ad1
GoldenSpamfish <ryanellis3@gmail.com>
2021-04-29 09:48:28 -0400

    local update (hopefully doesnt break anything)

--- test-scripts/google-sheets-html.js
+++ test-scripts/google-sheets-html.js
@@ -3,7 +3,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1v6i3MFlBfTom7p_WZHs-hK850D2pTg2kk4XJbrLlkd8/gviz/tq?output=html&usp=sharing');
-    query.setQuery('SELECT A, B, C, D label A "Scientific Name", B "Common Name", C "Link", D "Growth Form"');
+    query.setQuery('SELECT A, B, C, D, E label A "Scientific Name", B "Common Name", C "Link", D "Growth Form", E "Family"');
     query.send(handleQueryResponse);
 }
 
